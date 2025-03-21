327184e322cb9596bb561f749242f16d10864456
bentansusanto <tansusanto194@gmail.com>
2025-03-21 19:59:55 +0700

    add winston

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,5 +1,17 @@
 import { Global, Module } from '@nestjs/common';
-
+import {WinstonModule} from 'nest-winston'
+import winston from 'winston';
 @Global()
-@Module({})
+@Module({
+    imports: [
+        WinstonModule.forRoot({
+            level: 'debug',
+            format: winston.format.combine(
+              winston.format.timestamp(),
+              winston.format.json(),
+            ),
+            transports: [new winston.transports.Console()],
+          }),
+    ]
+})
 export class CommonModule {}
