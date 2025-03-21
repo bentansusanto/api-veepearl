d34a2d2e877e61f37badcfdd3ba63d61a9abf31e
bentansusanto <tansusanto194@gmail.com>
2025-03-21 22:50:28 +0700

    update entities

--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -59,7 +59,7 @@ export class AuthService {
         email: createReq.email,
         password: hashPassword,
         name: createReq.name,
-        role: createReq.role,
+        role: createReq.role || UserRole.CUSTOMER,
         otpCode,
         expOtp: new Date(expiry),
       })
