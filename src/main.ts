5824c98ac7f229469ee9a25166d21e12e4271300
bentansusanto <tansusanto194@gmail.com>
2025-03-28 16:16:58 +0700

    add new origin;

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  const origins = [
+   'http://localhost:3500',
+   'http://localhost:3700',
+   'https://veepearls.com',
+   'https://vee.my.id'
+  ]
   app.use(
     cors({
       origin: function (origin, callback) {
-        const allowedOrigins = ['http://localhost:3500'];
+        const allowedOrigins = origins
         if (!origin || allowedOrigins.includes(origin)) {
           callback(null, true);
         } else {
@@ -20,10 +26,10 @@ async function bootstrap() {
     }),
   );
   const port =
-  process.env.NODE_ENV === 'development'
-    ? process.env.PORT_DEV
-    : process.env.PORT_PROD;
-await app.listen(port);
-console.log(`Application is running port: ${port}`);
+    process.env.NODE_ENV === 'development'
+      ? process.env.PORT_DEV
+      : process.env.PORT_PROD;
+  await app.listen(port);
+  console.log(`Application is running port: ${port}`);
 }
 bootstrap();
